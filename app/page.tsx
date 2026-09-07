931a82c9c4194d08c14c6d4fd93a2d1bde7b6855
callie4k-appiah <callie4k@gmail.com>
2026-09-07 23:24:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useRef, useState } from "react";
 
-const platforms = ["Uber", "Bolt", "Staxi", "Welcome Pickups"];
+const platforms = ["Uber", "Bolt", "Andere platformen];
 
 const benefits = [
   {
